e7c93f9e1eefa2299a208275654ca4c152633536
Anders Ward <anders.s.ward@gmail.com>
2023-02-27 19:48:49 -0800

    Filtering Fix

--- TweetRead.py
+++ TweetRead.py
@@ -18,7 +18,14 @@ access_token = getenv('accessToken')
 access_token_secret = getenv('accessTokenSecret')
 bearer_token = getenv('bearerToken')
 
-DEFAULT_SEARCH_TERMS = ['California snow in February']
+CA_SNOW_SEARCH = "lang:en -is:retweet (\"california snow\" OR \"cali snow\") february" # english language, no retweets, 
+UCLA_SEARCH = "lang:en -is:retweet ucla OR #ucla OR @ucla"
+RONALDO_SEARCH = "to:Cristiano lang:en"
+VERIFIED_SEARCH = "is:verified lang:en"
+DEFAULT_STREAM_FILTERS = [CA_SNOW_SEARCH, 
+                          UCLA_SEARCH,
+                          RONALDO_SEARCH,
+                          VERIFIED_SEARCH]
 
 class TweetsListener(StreamingClient):
     # https://www.youtube.com/watch?v=8r5en18DOZQ
@@ -52,21 +59,28 @@ class TweetsListener(StreamingClient):
 
 
 
-def sendData(c_socket, search_terms = DEFAULT_SEARCH_TERMS):
+def sendData(c_socket, stream_filter_list = DEFAULT_STREAM_FILTERS):
 
     twitter_stream = TweetsListener(
         bearer_token,
         csocket=c_socket
     )
-    for term in search_terms:
-        """
-        If you are using an Academic Research Project_ at the Basic access level, 
-        you can use all available operators, can submit up to 1,000 concurrent rules, 
-        and can submit rules up to 1,024 characters long.
-        """
-        twitter_stream.add_rules(tweepy.StreamRule(term))
-
+    """
+    If you are using an Academic Research Project_ at the Basic access level, 
+    you can use all available operators, can submit up to 1,000 concurrent rules, 
+    and can submit rules up to 1,024 characters long.
+    """
+    # twitter api has persistent rules
+    current_rules = twitter_stream.get_rules()
+    to_delete = [x.id for x in current_rules.data]
+    twitter_stream.delete_rules(to_delete)
+    # enforce new rules
+    for stream_filter in stream_filter_list:
+        twitter_stream.add_rules(tweepy.StreamRule(stream_filter))
+    
     twitter_stream.filter()
+    # check
+    print(f"\nFiltering with the following rules {twitter_stream.get_rules()}\n")
 
 if __name__ == "__main__":
     s = socket.socket()         # Create a socket object
