c6e5c9bdebaeafecfbf8ab038ae61fbfa305b98e
Anders Ward <anders.s.ward@gmail.com>
2023-03-09 13:31:54 -0800

    Flask App Structural Outline

---
+++ flask_root/twitter.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def get_twitter_data(query):
+    return pd.DataFrame(
+        [
+            {
+                "tweet": "test",
+                "sentiment": 0
+            
+            }
+        ]
+    )
