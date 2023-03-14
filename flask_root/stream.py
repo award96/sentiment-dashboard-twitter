f3d2fd637fcff48474d7e73dd3e7e0955a863874
Anders Ward <anders.s.ward@gmail.com>
2023-03-13 22:52:19 -0700

    Fixed repeat tweet error on stream

--- flask_root/stream.py
+++ flask_root/stream.py
@@ -5,7 +5,6 @@ import json
 from os import getenv
 from dotenv import load_dotenv
 
-MAX_TWEETS = 24
 load_dotenv()
 
 # To set your enviornment variables in your terminal run the following line:
@@ -18,7 +17,6 @@ bearer_token = getenv('bearerToken')
 
 
 def bearer_oauth(r):
-    print("bearer_oath")
     """
     Method required by bearer token authentication.
     """
@@ -36,7 +34,6 @@ def get_rules():
         raise Exception(
             "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
         )
-    print("get_rules()")
     print(json.dumps(response.json()))
     return response.json()
 
@@ -58,11 +55,10 @@ def delete_all_rules(rules):
                 response.status_code, response.text
             )
         )
-    print("delete_all_rules")
     print(json.dumps(response.json()))
 
 
-def set_rules(search_term: str):
+def set_rules(search_term: str = "Oscars"):
     # You can adjust the rules if needed
     sample_rules = [
         {"value": f"lang:en -is:retweet #{search_term}"},
@@ -77,16 +73,13 @@ def set_rules(search_term: str):
         raise Exception(
             "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
         )
-    print("set_rules")
     print(json.dumps(response.json()))
 
 
 def get_stream():
-    tweet_count=0
     response = requests.get(
         "https://api.twitter.com/2/tweets/search/stream", auth=bearer_oauth, stream=True,
     )
-    print("get_stream")
     print(response.status_code)
     if response.status_code != 200:
         raise Exception(
@@ -96,27 +89,16 @@ def get_stream():
         )
     for response_line in response.iter_lines():
         if response_line:
-            while tweet_count <= MAX_TWEETS:
-                tweet_count += 1
-                json_response = json.loads(response_line)
-                res = json.dumps(json_response, indent=4, sort_keys=True)
-                #print(res)
-                yield res
+            json_response = json.loads(response_line)
+            yield json.dumps(json_response, indent=4, sort_keys=True)
 
 
 def main(search_term: str = "Oscars"):
-    print("main")
     rules = get_rules()
     delete_all_rules(rules)
     set_rules(search_term)
-    tc = 0
-    for tweet in get_stream():
-        tc+=1
-        if tc > MAX_TWEETS:
-            break
-        print(tweet)
-        yield tweet
+    yield from get_stream()
 
 
 if __name__ == "__main__":
-    main()
+    [print(x) for x in main()]
